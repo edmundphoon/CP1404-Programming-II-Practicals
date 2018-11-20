0a333c1edf296ffbc5fb1dc9c2048465f161f085
edmundpjc <edmundjiancheng.phoon@my.jcu.edu.au>
2018-11-20 13:40:08 +0800

    Arrange definitions and functions

--- Practical 3/temperatures_refactor.py
+++ Practical 3/temperatures_refactor.py
@@ -12,27 +12,28 @@ choice = input(">>> ").upper()
 
 def cel_to_fah():
     global celsius, fahrenheit
-    celsius = float(input("Celsius: "))
     fahrenheit = celsius * 9.0 / 5 + 32
-    print("Result: {:.2f} F".format(fahrenheit))
 
 
 def fah_to_cel():
     global fahrenheit, celsius
-    # TODO: Write this section to convert F to C and display the result
-    fahrenheit = float(input("Fahrenheit temperature: "))
     # Hint: celsius = 5 / 9 * (fahrenheit - 32)
     celsius = 5 / 9 * (fahrenheit - 32)
-    # Remove the "pass" statement when you are done. It's a placeholder.
-    # pass
-    print("Result: {:.2f} C".format(celsius))
+
 
 
 while choice != "Q":
     if choice == "C":
+        celsius = float(input("Celsius: "))
         cel_to_fah()
+        print("Result: {:.2f} F".format(fahrenheit))
     elif choice == "F":
+        # TODO: Write this section to convert F to C and display the result
+        fahrenheit = float(input("Fahrenheit temperature: "))
         fah_to_cel()
+        # Remove the "pass" statement when you are done. It's a placeholder.
+        # pass
+        print("Result: {:.2f} C".format(celsius))
     else:
         print("Invalid option")
     print(MENU)
