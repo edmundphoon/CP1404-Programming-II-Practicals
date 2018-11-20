d41733b68adcc56d92bd5ac067880db43262d425
edmundpjc <edmundjiancheng.phoon@my.jcu.edu.au>
2018-11-20 22:39:48 +0800

    Define score and main

---
+++ Practical 3/broken_score.py
@@ -0,0 +1,18 @@
+"""
+CP1404/CP5632 - Practical
+Pseudocode for broken score
+"""
+
+def total_score(x, y):
+    result = int(x) + int(y)
+    return result
+
+def main():
+    x = input("Enter a score: ")
+    y = input("Enter a score: ")
+    total_score(x, y)
+    print (total_score(x, y))
+
+main()
+
+
