b81ffd6e33764c3b3f426add9d82708e6fca8400
edmundpjc <edmundjiancheng.phoon@my.jcu.edu.au>
2018-11-27 11:08:33 +0800

    Append function

---
+++ Practical 4/five_num_exercise.py
@@ -0,0 +1,10 @@
+numbers = []
+
+for number in range(5):
+    number = numbers.append(int(input("Number: ")))
+
+print ("The first number is {:}".format(numbers[0]))
+print ("The last number is {:}".format(numbers[-1]))
+print ("The smallest number is {:}".format(min(numbers)))
+print ("The largest number is {:}".format(max(numbers)))
+print ("The average of the numbers is {:}".format(str(sum(numbers)/len(numbers))))
